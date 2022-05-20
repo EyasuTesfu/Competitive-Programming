836f19a12cf8179424fa8cb4a2b02389f6e36aa7
Joshua Tesfaye <jt272525@gmail.com>
2022-05-20 19:12:42 +0300

    better code for good pairs

--- Week 3/GoodPairs.py
+++ Week 3/GoodPairs.py
@@ -1,3 +1,6 @@
+from tkinter import N
+
+
 class Solution:
     def numIdenticalPairs(self, nums: list[int]) -> int:
         count = 0
@@ -6,3 +9,20 @@ class Solution:
                 if nums[i] == nums[j]:
                     count += 1
         return count
+
+#
+#
+# Best approach would be using a dictionary which would be O(n)
+
+
+class Solution:
+    def numIdenticalPairs(self, nums: list[int]) -> int:
+        good_pairs = 0
+        pairs = {}
+        for i in range(len(nums)):
+            if pairs.get(nums[i], None) == None:
+                pairs[nums[i]] = 1
+            else:
+                good_pairs += pairs[nums[i]]
+                pairs[nums[i]] += 1
+        return good_pairs
