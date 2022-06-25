92a96e5ff3438299ed916f1223209b5a88d2c3f3
Joshua Tesfaye <jt272525@gmail.com>
2022-06-25 13:12:02 +0300

    min stack

---
+++ week 8/minstack.py
@@ -0,0 +1,30 @@
+'''
+link to problem: https://leetcode.com/problems/min-stack/
+implemented using arrays/lists
+'''
+
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        self.min = min(self.stack)
+        return self.min
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
