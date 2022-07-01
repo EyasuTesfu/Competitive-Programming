3e25c3740ba8a2ea7b92a08fd468258fe93e74ee
Joshua Tesfaye <jt272525@gmail.com>
2022-07-01 14:26:59 +0300

    efficient next great element

--- week 8/NextGreatElement.py
+++ week 8/NextGreatElement.py
@@ -1,6 +1,7 @@
 '''
 Question link = https://leetcode.com/problems/next-greater-element-i/
-Solution: ___EASY___'''
+Solution1: ___EASY___BruteForce Solution
+Solution2: ___Medium__Stack Solution'''
 from typing import List
 
 
@@ -12,4 +13,21 @@ class Solution:
                 if _great > num:
                     great_element[nums1.index(num)] = _great
                     break
+        return great_element
+
+# Below is a faster solution O(n + m) time complexity and a O(m) space complexity using stack
+
+
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        num_index = {num: i for i, num in enumerate(nums1)}
+        stack = []
+        great_element = [-1] * len(nums1)
+        for num in nums2:
+            if stack and num > stack[-1]:
+                while(stack and num > stack[-1]):
+                    great_element[num_index[stack[-1]]] = num
+                    stack.pop()
+            if num in nums1:
+                stack.append(num)
         return great_element
