dbdb76d583efeabaafe3758f1d26076f0cc830b2
Joshua Tesfaye <jt272525@gmail.com>
2022-07-03 17:35:15 +0300

    sort corrected

--- Previously Done/Insertion sort1.py
+++ Previously Done/Insertion sort1.py
@@ -17,14 +17,17 @@ import sys
 def insertionSort1(n, arr):
     i = -2
     temp = arr[-1]
-    while(i >= -n-1):
+    while(i >= -n):
         if arr[i] > temp:
             arr[i+1] = arr[i]
+            print(str(arr).strip("[]").replace(",", ""))
+            if arr[i] == arr[0]:
+                arr[i] = temp
+                print(str(arr).strip("[]").replace(",", ""))
         else:
             arr[i+1] = temp
             print(str(arr).strip("[]").replace(",", ""))
             break
-        print(str(arr).strip("[]").replace(",", ""))
         i -= 1
 
 
