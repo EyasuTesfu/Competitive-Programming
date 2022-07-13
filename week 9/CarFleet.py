c8087b3d8c06a9770c8931145070b5a2245eb90d
Joshua Tesfaye <jt272525@gmail.com>
2022-07-13 15:21:05 +0300

    a

--- week 9/CarFleet.py
+++ week 9/CarFleet.py
@@ -1,3 +1,10 @@
+
+'''
+Question link = https://leetcode.com/problems/car-fleet/
+Solution: Use a monotonicly increasing stack on a sorted position, which contains a pair of position and speed to find out the time of arrival to the target, which means if the
+arrival time is less than or equal to the top of the stack, then the two positions must have colided at some point'''
+
+
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         stack = []
