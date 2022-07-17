a3c3f9074a2a04b7da087b77adffdec9b686af8a
Joshua Tesfaye <jt272525@gmail.com>
2022-07-17 14:10:14 +0300

    1422 ms, faster than 15.31%, 27.5 MB, less than 54.33%, o(n) time complexity, spcae complexity: O(n)

---
+++ week 9/MostWater.py
@@ -0,0 +1,26 @@
+'''
+area = distance(index_difference) * minimum height
+move right and left pointer based on which one is greater
+because the greater one will give a greater or equal area to the previous one
+but if we move one it will get even lower and we might not 
+get our potential area'''
+
+
+from typing import List
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        if len(height) == 2:
+            return min(height[0], height[1])
+        l = 0
+        r = len(height) - 1
+        most_water = 0
+        while(l != r):
+            area = (r-l) * (min(height[r], height[l]))
+            most_water = max(most_water, area)
+            if height[l] > height[r]:
+                r -= 1
+            else:
+                l += 1
+        return most_water
