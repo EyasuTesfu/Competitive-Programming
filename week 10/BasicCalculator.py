2f727343bdc13adf320b4de2b78effbcb0bec3fb
Joshua Tesfaye <jt272525@gmail.com>
2022-07-30 18:05:29 +0300

    118 ms, faster than 69.48% 21.2 MB, less than 5.73% time complextiy: O(n), space complexity: O(n)

---
+++ week 10/BasicCalculator.py
@@ -0,0 +1,32 @@
+'''
+Question link: https://leetcode.com/problems/basic-calculator-ii/'''
+
+
+class Solution:
+    def calculate(self, s: str) -> int:
+        result = 0
+        operator = ""
+        stack = []
+        curr = "0"
+        for i in range(len(s)):
+            _str = s[i]
+            if _str not in "+-/*" and _str != " ":
+                curr += _str
+            if _str in "+-/*" or i == len(s)-1:
+                if operator == "-":
+                    stack.append("-"+curr)
+                elif operator == "*":
+                    value = int(stack.pop())
+                    multiply = value * int(curr)
+                    stack.append(multiply)
+                elif operator == "/":
+                    value = int(stack.pop())
+                    divide = int(value / int(curr))
+                    stack.append(divide)
+                else:
+                    stack.append(curr)
+                operator = _str
+                curr = "0"
+        for val in stack:
+            result += int(val)
+        return result
