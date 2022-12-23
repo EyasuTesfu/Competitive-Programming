5737b26f0e9274144884508425a574ce228dcd4b
Joshua Tesfaye <jt272525@gmail.com>
2022-12-23 17:29:16 +0300

    what is your name?

---
+++ Categorized/A2SV/whats_your_name.py
@@ -0,0 +1,3 @@
+# problem link: https://www.hackerrank.com/challenges/whats-your-name/submissions/code/306006240
+def print_full_name(first, last):
+    print(f"Hello {first} {last}! You just delved into python.")
