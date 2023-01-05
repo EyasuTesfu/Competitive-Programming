225ea6cefafb93aa945435b92b8ed93d63214724
Joshua Tesfaye <jt272525@gmail.com>
2023-01-05 15:21:56 +0300

    python mod

---
+++ Categorized/A2SV/mod.py
@@ -0,0 +1,6 @@
+# problem link: https://www.hackerrank.com/challenges/python-power-mod-power/problem?isFullScreen=true
+a = int(input())
+b = int(input())
+m = int(input())
+print(pow(a, b))
+print(pow(a, b, m))
