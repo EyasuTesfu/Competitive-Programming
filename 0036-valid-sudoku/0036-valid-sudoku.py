a7d20c89a94fb2bede1e72ae8a0de5a6c190a615
Joshua Tesfaye <87969938+EyasuTesfu@users.noreply.github.com>
2023-01-20 19:22:16 +0300

    Time: 102 ms (74.11%), Space: 13.9 MB (31.67%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,44 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row = {}
+        column = {}
+        thr_by_thr = {}
+        i = j = r = c = 0
+        while(i < 9 and j < 9):
+            r = c = 0
+            while(r < 9 and c < 9):
+                if board[i][c].isdigit():
+                    if i in row.keys():
+                        if board[i][c] in row[i].keys():
+                            return False
+                        row[i][board[i][c]] = 1
+                    else:
+                        row[i] = {}
+                        row[i][board[i][c]] = 1
+                    if (i//3, c//3) not in thr_by_thr.keys():
+                        thr_by_thr[(i//3, c//3)] = {}
+                        thr_by_thr[(i//3, c//3)][board[i][c]] = (i,c)
+                    else:
+                        if board[i][c] in thr_by_thr[(i//3, c//3)].keys() and thr_by_thr[(i//3, c//3)][board[i][c]] != (i,c):
+                            return False
+                        thr_by_thr[(i//3, c//3)][board[i][c]] = (i,c)
+                if board[r][j].isdigit():
+                    if j in column.keys():
+                        if board[r][j] in column[j].keys():
+                            return False
+                        column[j][board[r][j]] = 1
+                    else:
+                        column[j] = {}
+                        column[j][board[r][j]] = 1
+                    if (r//3, j//3) not in thr_by_thr.keys():
+                        thr_by_thr[(r//3, j//3)] = {}
+                        thr_by_thr[(r//3, j//3)][board[r][j]] = (r,j)
+                    else:
+                        if board[r][j] in thr_by_thr[(r//3, j//3)].keys() and thr_by_thr[(r//3, j//3)][board[r][j]] != (r,j):
+                            return False
+                        thr_by_thr[(r//3, j//3)][board[r][j]] = (r,j)
+                r += 1
+                c += 1
+            i += 1
+            j += 1
+        return True
