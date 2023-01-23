f37edcf12d5c6eab4599062791c8ae64ed262475
EyasuTesfu <jt272525@gmail.com>
2023-01-23 19:16:57 +0300

    insertion sort

---
+++ HackerRank/insertion_sort.py
@@ -0,0 +1,15 @@
+def insertionSort1(n, arr):
+    i = -2
+    temp = arr[-1]
+    while (i >= -n):
+        if arr[i] > temp:
+            arr[i+1] = arr[i]
+            print(str(arr).strip("[]").replace(",", ""))
+            if arr[i] == arr[0]:
+                arr[i] = temp
+                print(str(arr).strip("[]").replace(",", ""))
+        else:
+            arr[i+1] = temp
+            print(str(arr).strip("[]").replace(",", ""))
+            break
+        i -= 1
