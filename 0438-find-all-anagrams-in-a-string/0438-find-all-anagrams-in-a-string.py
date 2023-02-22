c20acacea1dc64960d7d771840f29c3c1da49100
Joshua Tesfaye <87969938+EyasuTesfu@users.noreply.github.com>
2023-02-22 11:10:27 +0300

    Time: 234 ms (41.05%), Space: 15.2 MB (74.27%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -2,18 +2,24 @@ class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         res = []
         p_counter = Counter(p)
-        s2 = Counter(s[:len(p)])
-        if self.check_sub(p_counter, s2):
-                res.append(0)
-        i, j = 0, len(p)
+        s2 = Counter(s[:len(p) - 1])
+
+#         if self.check_sub(p_counter, s2):
+#                 res.append(0)
+                
+        i, j = 0, len(p) - 1
         while j < len(s):
-            s2[s[i]] -= 1
+            
             if s[j] in s2:
                 s2[s[j]] += 1
             else:
                 s2[s[j]] = 1
+        
             if self.check_sub(p_counter, s2):
-                res.append(i+1)
+                res.append(i)
+            
+            s2[s[i]] -= 1
+   
             i += 1
             j += 1
         return res
