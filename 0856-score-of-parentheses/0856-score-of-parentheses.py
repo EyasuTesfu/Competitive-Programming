021fcfe6202b3e7e598a5e19d001af00df962d75
Joshua Tesfaye <87969938+EyasuTesfu@users.noreply.github.com>
2023-02-28 15:28:22 +0300

    Time: 34 ms (51.19%), Space: 13.8 MB (49.79%) - LeetHub

--- 0856-score-of-parentheses/0856-score-of-parentheses.py
+++ 0856-score-of-parentheses/0856-score-of-parentheses.py
@@ -1,11 +1,5 @@
 class Solution:
     def scoreOfParentheses(self, s: str) -> int:
-        # () == 1
-        # ()() = 2
-        # (()) = 2 * 1 = 2
-        # ((())) = 8 and (()()) = 4
-        # stack = [0, 2, 2], stack = [0, 1, 0, 1, 0, 2, 4] 2 * sum(min_stack)
-        # will hold a count
         stack = [0]
         for i in range(len(s)):
             if s[i] == '(':
