68eb623f038aeab892f3d1d0e153641f21051fa4
Joshua Tesfaye <87969938+EyasuTesfu@users.noreply.github.com>
2023-02-28 16:11:24 +0300

    Time: 32 ms (64.52%), Space: 13.8 MB (49.79%) - LeetHub

--- 0856-score-of-parentheses/0856-score-of-parentheses.py
+++ 0856-score-of-parentheses/0856-score-of-parentheses.py
@@ -1,10 +1,15 @@
 class Solution:
     def scoreOfParentheses(self, s: str) -> int:
+        # '(' is the first value so for that we initially have a zero value
+        # and also for the operation stack[-1] += (max(1, 2 *stack.pop()))
+        # to avoid if
         stack = [0]
         for i in range(len(s)):
-            if s[i] == '(':
+            if s[i] == "(":
                 stack.append(0)
-            else:
-                popped = stack.pop()
-                stack[-1] += max(2*popped, 1)
-        return stack.pop()
+            elif s[i] == ")":
+                # change the inside value so that it becomes greater than 1
+                # when multiplied with 2
+                holder = stack.pop()
+                stack[-1] += max(2 * holder, 1)
+        return stack[-1]
