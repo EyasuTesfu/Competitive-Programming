482e537abd1a0790cd63073fbce004be18aefc0f
Joshua Tesfaye <87969938+EyasuTesfu@users.noreply.github.com>
2023-03-10 09:39:09 +0300

    Time: 351 ms (78.68%), Space: 16 MB (43.77%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -2,11 +2,12 @@ class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         values = [i for i in range(n+1)]
         res = []
-        def backtracker(arr, idx):
-            if len(arr) == k:
-                res.append(arr)
+        def backtracker(arr, idx, slot_index):
+            if slot_index == k:
+                res.append(arr[:])
                 return
             for i in range(idx, n+1):
-                backtracker(arr + [values[i]], i+1)
-        backtracker([] ,1)
+                arr[slot_index] = i
+                backtracker(arr, i+1, slot_index+1)
+        backtracker([0]*k ,1, 0)
         return res
