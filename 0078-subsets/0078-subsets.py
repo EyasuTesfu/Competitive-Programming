3910fe83855b4a45716a64ce9c664f298608c6cc
Joshua Tesfaye <87969938+EyasuTesfu@users.noreply.github.com>
2023-03-14 18:14:34 +0300

    Time: 35 ms (69.07%), Space: 14 MB (77.82%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,13 +1,18 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        def cascade(arr, idx):
-            if idx == len(nums):
-                return arr
+        res = []
+        def dfs(start = 0, curr = []):
+            res.append(curr)
             
-            new_arr = []
-            for i in range(2**idx):
-                new_arr.append(arr[i]+[nums[idx]])
+            for i in range(start, len(nums)):
+                # go through
+                curr.append(nums[i])
+                # call the function
+                dfs(i + 1, curr[:])
+                # backtrack
+                curr.pop()
+        dfs(0, [])
+        return res
+                
             
-            return cascade(arr+new_arr, idx + 1)
-        return cascade([[]], 0)
                 
