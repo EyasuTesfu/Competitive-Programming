615b9936aa6b5cf73ce61262e46adfc3a4bc0524
Joshua Tesfaye <87969938+EyasuTesfu@users.noreply.github.com>
2023-03-22 00:34:40 +0300

    Time: 432 ms (52.13%), Space: 19.7 MB (12.53%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.py
@@ -0,0 +1,15 @@
+class StockSpanner:
+
+    def __init__(self):
+        self.stack = [] # monotonically decreasing stack, holding how many smaller where just before this one
+
+    def next(self, price: int) -> int:
+        count = 1
+        while self.stack and self.stack[-1][0] <= price:
+            count += self.stack.pop()[1]
+        self.stack.append([price, count])
+        return count
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
