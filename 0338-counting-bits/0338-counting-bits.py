43d52c81cf7c3957a2201f7e754d43d0dd9fc271
Joshua Tesfaye <87969938+EyasuTesfu@users.noreply.github.com>
2023-03-30 00:14:22 +0300

    Time: 87 ms (62.74%), Space: 20.8 MB (65.78%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -2,5 +2,5 @@ class Solution:
     def countBits(self, n: int) -> List[int]:
         res = [0]*(n+1)
         for i in range(1, n+1):
-            res[i] = res[i//2] + i % 2
+            res[i] = res[i >> 1] + i % 2
         return res
