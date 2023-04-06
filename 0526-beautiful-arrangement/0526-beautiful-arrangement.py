ecb1f2a72d7cf919288c49408509be2068dc8f33
Joshua Tesfaye <87969938+EyasuTesfu@users.noreply.github.com>
2023-04-06 18:07:08 +0300

    Time: 1962 ms (16.67%), Space: 14 MB (34.72%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.py
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.py
@@ -1,18 +1,17 @@
 class Solution:
     def countArrangement(self, n: int) -> int:
-        res = []
-        nums = [i for i in range(1,n+1)]
+        self.res = 0
         def bfs(arr, vis = 0):
-            if len(arr) == len(nums):
-                res.append(arr)
+            if arr == n:
+                self.res += 1
                 return
-            for i in range(len(nums)):
+            for i in range(1,n+1):
                 if not (vis & (1 << i)):
-                    if nums[i] % (len(arr)+1) == 0 or (len(arr) + 1) % nums[i] == 0:
-                        arr.append(nums[i])
+                    if i % (arr+1) == 0 or (arr + 1) % i == 0:
+                        arr += 1
                         vis = vis | (1 << (i))
-                        bfs(arr[:], vis)
+                        bfs(arr, vis)
                         vis = vis ^(1 << (i))
-                        arr.pop()
-        bfs([])
-        return len(res)
+                        arr -= 1
+        bfs(0)
+        return self.res
