8caf92509244bccba2cf6386f397f66e4e6caae4
Joshua Tesfaye <87969938+EyasuTesfu@users.noreply.github.com>
2023-05-12 07:41:06 +0300

    added find-k-pairs-with-smallest-sums.python

--- find-k-pairs-with-smallest-sums.py
+++ find-k-pairs-with-smallest-sums.py
@@ -3,16 +3,12 @@ class Solution:
         heap = []
         heapq.heappush(heap, (nums1[0]+ nums2[0], 0,0))
         res= []
-        vis = set()
-        vis.add((0,0))
         while heap and k > 0:
             val, i, j = heapq.heappop(heap)
             res.append([nums1[i], nums2[j]])
             k -= 1
-            if i+1 < len(nums1) and (i+1, j) not in vis:
+            if i+1 < len(nums1):
                 heapq.heappush(heap, (nums1[i+1] + nums2[j], i+1, j))
-                vis.add((i+1,j))
-            if j + 1 < len(nums2) and (i, j+1) not in vis:
+            if j + 1 < len(nums2):
                 heapq.heappush(heap, (nums1[i] + nums2[j+1], i, j+1))
-                vis.add((i,j+1))
         return res
