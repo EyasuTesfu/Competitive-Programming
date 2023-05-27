3fa0e12249d30a37042fbb63a800ef3e3f381006
Joshua Tesfaye <87969938+EyasuTesfu@users.noreply.github.com>
2023-05-28 01:17:50 +0300

    added n-th-tribonacci-number.python

---
+++ n-th-tribonacci-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        memo = {}
+        def trib(n):
+            if n == 0: return 0
+            if n == 1 or n == 2: return 1
+            if n not in memo:
+                memo[n] = trib(n-1) + trib(n-2) + trib(n-3)
+            return memo[n]
+        return trib(n)
