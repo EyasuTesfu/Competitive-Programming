8ea202c867029092a6c19fe924f69ca88e95c273
EyasuTesfu <jt272525@gmail.com>
2023-09-28 15:10:26 +0300

    codeforces code

---
+++ B_Ehab_Is_an_Odd_Person.py
@@ -0,0 +1,14 @@
+n = int(input())
+arr = list(map(int, input().split()))
+even = 0
+odd = 0
+for i in range(len(arr)):
+    if arr[i] % 2 == 0:
+        even += 1
+    else:
+        odd += 1
+if even * odd:
+    arr.sort()
+    print(*arr)
+else:
+    print(*arr)
