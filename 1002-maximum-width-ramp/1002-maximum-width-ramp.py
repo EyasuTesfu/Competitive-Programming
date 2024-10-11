1bfece528195aba5e8a65be6c4ae88019419c126
Joshua Tesfaye <87969938+EyasuTesfu@users.noreply.github.com>
2024-10-11 12:31:29 +0300

    Time: 475 ms (5.28%), Space: 27.6 MB (6.18%) - LeetHub

--- 1002-maximum-width-ramp/1002-maximum-width-ramp.py
+++ 1002-maximum-width-ramp/1002-maximum-width-ramp.py
@@ -1,17 +1,16 @@
 class Solution:
     def maxWidthRamp(self, nums: List[int]) -> int:
-        n = len(nums)
-        indices = [i for i in range(n)]
+        nums_ind = [(nums[i], i) for i in range(len(nums))]
+        nums_ind.sort()
+        min_ind = nums_ind[0][1]
+        res = 0
+        for val, ind in nums_ind:
+            res = max(res, ind - min_ind)
+            min_ind = min(min_ind, ind)
+        return res
+            
+            
 
-        # Sort indices based on corresponding values in nums and ensure stability
-        indices.sort(key=lambda i: (nums[i], i))
+            
+                
 
-        min_index = n  # Minimum index encountered so far
-        max_width = 0
-
-        # Calculate maximum width ramp
-        for i in indices:
-            max_width = max(max_width, i - min_index)
-            min_index = min(min_index, i)
-
-        return max_width
